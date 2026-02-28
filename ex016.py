236c968e51e014ef41d911916667f128eea32be0
Judá Sales <juda_sales@hotmail.com>
2026-02-28 00:27:44 -0300

    Create ex016.py

---
+++ ex016.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+import math
+
+num = float(input("Digite um numero: "))
+print(f"O Numero {num} tem a porção inteira {math.floor(num)}.")
