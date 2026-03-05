8b3baec6d5ad43f03a87b427eddc1484c7f15286
Judá Sales <juda_sales@hotmail.com>
2026-03-05 15:31:28 -0300

    Update ex021.py

--- ex021.py
+++ ex021.py
@@ -2,7 +2,8 @@
 
 # Importa a biblioteca do pygame
 import pygame
-
+opcao = 0
+cont = 0
 # Iniciar o mixer do pygame
 pygame.mixer.init()
 
@@ -15,5 +16,17 @@ pygame.mixer.music.set_volume(0.8)
 # Reproduz o audio
 pygame.mixer.music.play()
 
-# Manter o programa rodando enquanto a musica toca
-input("Pressione Enter para Parar...")
+# Loop para ficar rodando um menu de opções
+while cont == 0:
+    opcao = int(input("Digite 1 para pausar, 2 para continuar e 0 para parar: "))
+    if opcao == 1:
+        pygame.mixer.music.pause()
+        cont = 0
+    elif opcao == 2:
+        pygame.mixer.music.unpause()
+        cont = 0
+    elif opcao == 0:
+        cont = 1
+else:
+    print("Você encerrou a musica: ")
+          
